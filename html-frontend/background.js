aa69d1f6c2a439514201a4efaa040ab01fe8437f
Ch1cken-Man <camscud@gmail.com>
2022-12-15 17:42:52 -0600

    parsed+transferred, need to make the order confir mation look nice

--- html-frontend/background.js
+++ html-frontend/background.js
@@ -7,10 +7,13 @@ var currentOrder = [0,0,0,0,0,0];
 var priceKey = [2.75,2.75,2.75,2.75,2.75,2.75]
 totalPrice = 0
 
+//changes a label's text
 function changeLabel(text, labelid){
     document.getElementById(labelid).textContent = text;
 }
 
+//frontpage.html used-
+//calculates order total and updates label at bottom
 function calculateTotal(){
     totalPrice = 0;
     for(let i = 0; i <currentOrder.length; i++){
@@ -20,10 +23,9 @@ function calculateTotal(){
         alert("you have ordered $100+ dollars of doughnuts. proceed??");
     }
     changeLabel(("total: $"+totalPrice), 'order-total');
-
 }
 
-
+//the function that changes the order when the add or remove button is pressed
 function changeOrder(doughnutCode, sign, labelid){
     var expectedOrder = currentOrder[doughnutCode] + sign;
     if(expectedOrder>=0){
@@ -35,22 +37,32 @@ function changeOrder(doughnutCode, sign, labelid){
 }
 
 
-
+//takes the currentOrder to the confirmation page
 function exitToConfirmationPage(){
-    sessionStorage.setItem("carriedOrder",currentOrder);
+    stringedOrder = JSON.stringify(currentOrder);
+    sessionStorage.setItem("carriedOrder",stringedOrder);
     window.location.href='confirmationPage.html';
     
 }
 
 
 
-
+//gets currentOrder from the previous page
+//adds the items in the order to a list to show what you choose.
 function loadConfirmationPage(){
-    currentOrder = sessionStorage.getItem("carriedOrder");
+    unparsedOrder = sessionStorage.getItem("carriedOrder");
+    currentOrder = JSON.parse(unparsedOrder);
+
     orderLength = currentOrder.length;
     for (var i = 0; i<orderLength; i++){
-        alert("works");    
+        if(currentOrder[i]>0){
+            addDoughnutToOrderConfirmation("div", currentOrder[i]);
+        }
     }
+}
+
+function exitToPaymentPage(){
+    alert("exiting to payment page");
 }
 
 function loadPaymentPage(){
